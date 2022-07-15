b36890717e108da9afd10c254d95bf1c22e23ee1
kdw8309 <kdw8309@gmail.com>
2022-07-15 15:59:23 +0900

    java

---
+++ src/MyMain/명명법.java
@@ -0,0 +1,25 @@
+package MyMain;
+
+public class 명명법 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//예약어			:	이미 생성된 명령(자료형/제어문/기타)
+		//사용자 정의 명칭	:	클래스명/메소드명/변수명/상수명
+		
+		System.out.println("--[사용자 정의 명칭 부여시 고려사항]--");
+		System.out.println("1. 숫자로 시작하면 안된다");
+		
+		//int 1kor; 이런 식이면 오류뜸 
+		int _1_kor;//이건 됨
+		
+		System.out.println("2. 중간에 공백 띄우면 안됨");
+		System.out.println("3. 특수문자 안됨");
+		System.out.println("4. 예약어 사용 불가");
+		
+		
+		
+	}
+
+}
